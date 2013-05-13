e8c218f3ff5adace2cf8e3349c55e56283a546ac
Mark Howlett <m.a.howlett@talk21.com>
2013-05-13 06:55:49 +0000

    start building gpio daemon - stop not working yet

---
+++ gpio-daemon.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import sys, time
+import socket
+import os
+import config as conf
+import ledLibrary as ledLib
+import tempLibrary as tempLib
+
+from daemon import daemon
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = int(conf.config['RPCServer']['Port'])
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((HOST, PORT))
+s.listen(1)
+
+class MyDaemon(daemon):
+        def run(self):
+            with ledLib.led:
+                while True:
+                    conn, addr = s.accept()
+                    #print('Connected by', addr)
+                    data = conn.recv(1024)
+                    stringData = data.decode(conf.config['encoding'])
+                    #
+                    # Handle commands
+                    #
+
+                    #default behaviour echo sent message back
+                    response = stringData
+                    #led on
+                    if stringData.upper() == "LEDON":
+                        ledLib.ledon()
+                        response = 'Led is now on'
+
+                    #led off
+                    if stringData.upper() == "LEDOFF":
+                        ledLib.ledoff()
+                        response = 'Led is now off'
+
+                    #led status
+                    if stringData.upper() == "LEDSTATUS":
+                        answer = ledLib.ledstatus()
+                        response = 'Led is ' + answer
+
+                    #temp (current)
+                    if stringData.upper() == "TEMPCURRENT":
+                        answer = tempLib.tempCurrent()
+                        response = 'Temperature is ' + answer + ' degrees centigrade'
+                    #help
+                    if stringData.upper() == "HELP":
+                        response = response + 'Commands are case insensitive,'
+                        response = response + 'ledon - Switch led on,'
+                        response = response + 'ledoff - Switch led off,'
+                        response = response + 'exit - close server,'
+                        response = response + 'help - This help,'
+
+                    #exit
+                    if stringData.upper() == "EXIT":
+                        break
+
+                    conn.sendall(response.encode(conf.config['encoding']))
+                conn.close()
+        def stop(self):
+            conn.close()
+            super(MyDaemon,self).stop()
+
+if __name__ == "__main__":
+        daemon = MyDaemon('/tmp/daemon-example.pid')
+        if len(sys.argv) == 2:
+                if 'start' == sys.argv[1]:
+                        daemon.start()
+                elif 'stop' == sys.argv[1]:
+                        daemon.stop()
+                elif 'restart' == sys.argv[1]:
+                        daemon.restart()
+                #for debugging purposes run in foreground
+                elif 'run' == sys.argv[1]:
+                        daemon.run()
+                else:
+                        print("Unknown command")
+                        sys.exit(2)
+                sys.exit(0)
+        else:
+                print("usage: %s start|stop|restart" % sys.argv[0])
+                sys.exit(2)
+
