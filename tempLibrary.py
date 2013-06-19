76b493d106e5c7f2f36369b07d8c317849f4fec7
Mark Howlett <m.a.howlett@talk21.com>
2013-06-19 20:45:00 +0000

    add mysqldb code for temp logger - needs to be converted to connector for py3

--- tempLibrary.py
+++ tempLibrary.py
@@ -4,6 +4,11 @@
 import os
 import sys
 import config as conf
+#import pymysql as mdb
+import datetime
+import time
+
+con = None
 
 def tempCurrent():
 
@@ -21,4 +26,21 @@ def tempCurrent():
         return output
 
 def tempLog():
-	return True
+        output="" 
+        try:
+            #con = mdb.connect('localhost','root',config.dbPassword,'test')
+            #cur = con.cursor()
+
+            temperature=tempCurrent()
+                
+            currdatetime = datetime.datetime.now().isoformat()
+            statement  = "INSERT INTO temps(temp,time) VALUES(%f,'%s')" % (temperature,currdatetime)
+            #cur.execute(statement)
+            #con.commit()
+            output = True
+        except mdb.Error as e:
+                output = False
+        finally:
+                if con:
+                        con.close()
+        return output
